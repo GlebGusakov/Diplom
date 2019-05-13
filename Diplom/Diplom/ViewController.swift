36624849589b18458df2a2b0f3834e681123de3c
Глеб Гусаков <>
2019-05-13 21:24:59 +0600

    Start work

---
+++ Diplom/Diplom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diplom
+//
+//  Created by Глеб Гусаков on 13/05/2019.
+//  Copyright © 2019 Глеб Гусаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
